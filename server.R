f65a74d829ac4878d06c108ff4c2a771207d5843
Steven <stevenjoelbrey@gmail.com>
2017-04-11 11:01:24 -0600

    complete rebuild of the look of the app and how layers are added to map. Now "groups" are used to add layers to the existing dates map.

--- server.R
+++ server.R
@@ -7,10 +7,10 @@
 load("data/hysplitPoints_land_both.RData") 
 load("data/fireOccurrence.RData")
 
-
 # TODO: Move the creation of the map outside of of events so it is never redrawn
 # http://stackoverflow.com/questions/37433569/changing-leaflet-map-according-to-input-without-redrawing
-
+# THIS ONE: https://rstudio.github.io/leaflet/showhide.html
+# Good for putting items on map https://rstudio.github.io/leaflet/shiny.html
 
 # Connect to a fire png logo and make small enough that plotting on map looks 
 # nice
@@ -52,127 +52,124 @@ shinyServer(function(input, output, session) {
     # Get the desired smoke plumes for plotting
     ###########################################
     s <- input$plumeDate
-    mergePlumes <- input$mergePlumes
+    #mergePlumes <- input$mergePlumes
     yyyymmdd <- str_replace_all(s, "-", "")
-    
-    # Do we want to load merged plumes or individual? 
-    if(mergePlumes == "show individual plumes"){
-      
-      plumeFile <- paste0('data/smoke/', yyyymmdd, "_hms_smoke.RData")
-      
-    } else if(mergePlumes == "merge overlapping"){
-      
-      plumeFile <- paste0('data/smoke/', yyyymmdd, "_merged_smoke.RData")
-      
-    }
-    
+    plumeFile <- paste0('data/smoke/', yyyymmdd, "_hms_smoke.RData")
+    plumeFile <- paste0('data/smoke/', yyyymmdd, "_hms_smoke.RData")
     smokePoly <- get(load(plumeFile))
     
+    ###########################################
+    # Handle HYSPLIT Points (hp)
+    ###########################################
+    dateSelect <- as.POSIXct(yyyymmdd, format="%Y%m%d", tz="UTC")
+    hpDates    <- hysplitPoints_land$Date
+    dateMask   <- hpDates == dateSelect
+    
+    # subset to selected date only
+    hp_df <- hysplitPoints_land[dateMask, ]
+    hp_lon <- as.numeric(hp_df$Lon)
+    hp_lat <- as.numeric(hp_df$Lat)
     
     ###########################################
-    # put map layers together
+    # Handle PM25 Monitors
     ###########################################
-    m = leaflet() %>%  
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)
-                       ) 
-    m = m %>% setView(lng=-100, lat=40, zoom=4)
-    m = m %>% addPolygons(data = smokePoly, fillColor="gray47", color="gray47") 
-    m = m %>% addScaleBar()
+    year <- str_sub(yyyymmdd,1,4)
+    monitorFile <- paste0("data/AQS/PM25/PM25_",year,".RData")
+    load(monitorFile) # loads "AQ_df" of class dataframe
     
-    if(input$plotPM25=="show"){
-      
-        year <- str_sub(yyyymmdd,1,4)
-        monitorFile <- paste0("data/AQS/PM25/PM25_",year,".RData")
-        load(monitorFile) # loads "AQ_df" of class dataframe
-      
-        # subset to this date and get rid of NA AQI
-        dateMask <- AQ_df$Date.Local == as.POSIXct(s, tz="UTC")
-        missingAQIMask <- !is.na(AQ_df$AQI)
-        PM_df    <- AQ_df[dateMask & missingAQIMask,]
-      
-        m = m %>% addCircleMarkers(
-                    lng=PM_df$Longitude,
-                    lat=PM_df$Latitude,
-                    color=PM_df$AQIColor,
-                    radius=10,
-                    fillOpacity=0.8,
-                    stroke=FALSE,
-                    label=paste("PM25 AQI =", PM_df$AQI ,"(",
-                                as.character(PM_df$Arithmetic.Mean), "ug/m2)")
-                  )
-    }
+    # subset to this date and get rid of NA AQI
+    dateMask <- AQ_df$Date.Local == dateSelect
+    missingAQIMask <- !is.na(AQ_df$AQI)
+    PM_df    <- AQ_df[dateMask & missingAQIMask,]
     
     ###########################################
-    # Show fire locations
+    # Handle CO Monitors 
     ###########################################
-    if (input$plotFires == "show"){
-      
-      dateSelect <- as.POSIXct(input$plumeDate, tz="UTC")
-      print(paste("dateSelect", dateSelect))
-      hpDates    <- hysplitPoints_land$Date
-      dateMask   <- hpDates == dateSelect 
-      
-      # subset to selected date only 
-      hysplitPoints_land <- hysplitPoints_land[dateMask, ]
-      
-      lon <- as.numeric(hysplitPoints_land$Lon)
-      lat <- as.numeric(hysplitPoints_land$Lat)
-      
-      
-      m = m %>% addMarkers(
-        lng=lon,
-        lat=lat,
-        clusterOptions = markerClusterOptions(),
-        icon = fireIcons,
-        label= paste(hysplitPoints_land$ModisGroupName, 
-                     "| Duration:", 
-                     as.numeric(str_sub(hysplitPoints_land$Dur,1,2)), 
-                     #hysplitPoints_land$Dur,
-                     "hrs")
-      ) 
-    }
+    year <- str_sub(yyyymmdd,1,4)
+    monitorFile <- paste0("data/AQS/CO/CO_",year,".RData")
+    load(monitorFile) # loads "AQ_df" of class dataframe
     
-    #########################################
-    # Handle addition of CO monitor plotting 
-    #########################################
-    if(input$plotCO=="show"){
+    # subset to this date and get rid of NA AQI
+    dateMask <- AQ_df$Date.Local == dateSelect
+    missingAQIMask <- !is.na(AQ_df$AQI)
+    CO_df    <- AQ_df[dateMask & missingAQIMask,]
+    
+    ###########################################
+    # Handle Ozone Monitors 
+    ###########################################
+    monitorFile <- paste0("data/AQS/ozone/ozone_",year,".RData")
+    load(monitorFile) # loads "AQ_df" of class dataframe
+    
+    # subset to this date and get rid of NA AQI
+    dateMask <- AQ_df$Date.Local == dateSelect
+    missingAQIMask <- !is.na(AQ_df$AQI)
+    ozone_df    <- AQ_df[dateMask & missingAQIMask,]
+    
+    ###########################################
+    # Handle USFS Fires
+    ###########################################
+    fireStart <- fire_data$DISCOVERY_DATE
+    fireEnd   <- fire_data$CONT_DATE
+    
+    # fdf = fireDataFrame
+    m1 <- dateSelect >= fireStart 
+    m2 <- dateSelect <= fireEnd
+    dateMask  <- m1 & m2
+    fdf <- fire_data[dateMask,]
+    
+    ###########################################
+    # Create map layers and toggles
+    ###########################################
+    map <- leaflet(smokePoly) %>%
       
-      year <- str_sub(yyyymmdd,1,4)
-      monitorFile <- paste0("data/AQS/CO/CO_",year,".RData")
-      load(monitorFile) # loads "AQ_df" of class dataframe
+      setView(lng=-100, lat=40, zoom=4) %>%
+      addScaleBar() %>%
       
-      # subset to this date and get rid of NA AQI
-      dateMask <- AQ_df$Date.Local == as.POSIXct(s, tz="UTC")
-      missingAQIMask <- !is.na(AQ_df$AQI)
-      CO_df    <- AQ_df[dateMask & missingAQIMask,]      
+      # Base groups
+      addTiles(group = "OSM (default)") %>%
+      addProviderTiles(providers$Stamen.Toner, group = "Toner") %>%
+      addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite") %>%
       
-      m = m %>% addCircleMarkers(
+      # Overlay groups
+      addPolygons(data = smokePoly, fillColor="gray47", color="gray47", 
+                  group="HMS Smoke Plumes") %>%
+      
+      addMarkers(
+            lng=hp_lon,
+            lat=hp_lat,
+            clusterOptions = markerClusterOptions(),
+            icon = fireIcons,
+            label= paste(hp_df$ModisGroupName,
+                         "| Duration:",
+                         as.numeric(str_sub(hp_df$Dur,1,2)),
+                         "hrs"),
+            group="HMS Fires"
+            ) %>%
+      
+      addCircleMarkers(
+        lng=PM_df$Longitude,
+        lat=PM_df$Latitude,
+        color=PM_df$AQIColor,
+        radius=10,
+        fillOpacity=0.8,
+        stroke=FALSE,
+        label=paste("PM25 AQI =", PM_df$AQI ,"(",
+                    as.character(PM_df$Arithmetic.Mean), "ug/m2)"),
+        group="PM25 Monitors"
+      ) %>%
+      
+      addCircleMarkers(
         lng=CO_df$Longitude,
         lat=CO_df$Latitude,
         color=CO_df$AQIColor,
         radius=10,
         fillOpacity=0.8,
         stroke=FALSE,
-        label=paste("CO =",as.character(CO_df$Arithmetic.Mean), "ppm")
-      )
-    }
-    
-    #########################################
-    # Handle addition of Ozone monitor plotting 
-    #########################################
-    if(input$plotOzone == "show"){
+        label=paste("CO =",as.character(CO_df$Arithmetic.Mean), "ppm"),
+        group="CO Monitors"
+      ) %>%
       
-      year <- str_sub(yyyymmdd,1,4)
-      monitorFile <- paste0("data/AQS/ozone/ozone_",year,".RData")
-      load(monitorFile) # loads "AQ_df" of class dataframe
-      
-      # subset to this date and get rid of NA AQI
-      dateMask <- AQ_df$Date.Local == as.POSIXct(s, tz="UTC")
-      missingAQIMask <- !is.na(AQ_df$AQI)
-      ozone_df    <- AQ_df[dateMask & missingAQIMask,]      
-      
-      m = m %>% addCircleMarkers(
+      addCircleMarkers(
         lng=ozone_df$Longitude,
         lat=ozone_df$Latitude,
         color=ozone_df$AQIColor,
@@ -180,57 +177,37 @@ shinyServer(function(input, output, session) {
         fillOpacity=0.8,
         stroke=FALSE,
         label=paste("O3 AQI =", as.character(ozone_df$AQI), "(MDA8 =",
-                    as.character(ozone_df$X1st.Max.Value * 1000), "ppb)")
+                    as.character(ozone_df$X1st.Max.Value * 1000), "ppb)"),
+        group="Ozone Monitors"
         
-      )
-    }
-    
-
-    # Must "return?" map object
-    m
-  })
-
-  observeEvent(input$plotUSFSFires,{
-
-    # Create a mask of active reported fires
-    s <- input$plumeDate
-    yyyymmdd <- str_replace_all(s, "-", "")
-    print(s)
-    dateSelect <- as.POSIXct(yyyymmdd, format="%Y%m%d", tz="UTC")
-    print(dateSelect)
-    
-    print("About to read from fire_data dates")
-    fireStart <- fire_data$DISCOVERY_DATE
-    fireEnd   <- fire_data$CONT_DATE
-    
-    #print("About to enter if statement")
-    # Make sure we have data in range
-    #if ( max(fireEnd, na.rm=TRUE)  <= dateSelect){
+      ) %>%
       
-    #print("inside if statement")
-      
-    # fdf = fireDataFrame
-    m1 <- dateSelect >= fireStart 
-    m2 <- dateSelect <= fireEnd
-    dateMask  <- m1 & m2
-    fdf <- fire_data[dateMask,]
-      
-    #print(dim(fdf))
-      
-    leafletProxy("mymap") %>%
       addMarkers(
+        layerId="USFSFires",
         lng=fdf$LONGITUDE,
         lat=fdf$LATITUDE,
         clusterOptions = markerClusterOptions(),
-        #icon = fireIcons,
-        label= paste(fdf$FIRE_NAME,", \n cuase:",fdf$STAT_CAUSE_DESCR)
-        )
+        label= paste(fdf$FIRE_NAME,", \n cuase:",fdf$STAT_CAUSE_DESCR),
+        group="USFS Reported Fires"
+      ) %>%
       
-    #}
+      # Layers control
+      addLayersControl(
+        position="topleft",
+        baseGroups = c("OSM (default)", "Toner", "Toner Lite"),
+        overlayGroups = c("HMS Smoke Plumes","HMS Fires", "PM25 Monitors", 
+                          "CO Monitors", "Ozone Monitors","USFS Reported Fires"),
+        options = layersControlOptions(collapsed = FALSE)
+      )
     
+    loading <- "False"
+    map
     
 
   })
-    
 
+
+
+  
+  
 })
--- ui.R
+++ ui.R
@@ -4,67 +4,44 @@ library(shiny)
 library(leaflet)
 library(stringr)
 
+loading <- "True"
 
 shinyUI(fluidPage(
-
-  headerPanel("NESDIS Hazard Mapping System Smoke Impact Explorer"),
+  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   
-  sidebarPanel(
-              h3("Data layers"),
-              
-              dateInput(inputId="plumeDate", label="Analysis Date", 
-                         value = "2012-06-13", 
-                         min = "2005-08-05", max = "2015-12-30",
-                         format = "yyyy-mm-dd", startview = "month",
-                         language = "en", width = "100%"),
-               
-               radioButtons("plotFires", "Plot HMS Fires", c("show", "hide"), 
-                            selected = "hide", inline = TRUE),
-               
-               radioButtons("plotPM25", "PM2.5 Monitors", c("show", "hide"), 
-                            selected = "hide", inline = TRUE),
-              
-               radioButtons("plotCO", "CO Monitors", c("show", "hide"), 
-                            selected = "hide", inline = TRUE),
-              
-               radioButtons("plotOzone", "Ozone Monitors", c("show", "hide"), 
-                            selected = "hide", inline = TRUE),
-               
-               radioButtons("mergePlumes", "", 
-                            c("show individual plumes", "merge overlapping"), 
-                            selected = "show individual plumes", inline = TRUE),
-              
-              actionButton("plotUSFSFires", "Reported Fires"),
-              
-               conditionalPanel(
-                 condition = "input.plotPM25 == 'PM2.5 FRM/FEM Mass AQI' | input.plotCO == 'CO AQI' | input.plotOzone == 'Ozone AQI'",
-                 img(src="aqi_legend.png", align = "left")
-               ),
-               
-              #h3("USFS Fire occurance"),
-              
-              width = 3
-            
+  headerPanel("Smoke Impact Explorer"),
+  
+  # conditionalPanel(condition = "loading== 'True'",
+  #                  img="ajax_loader_blue_512.gif"),
+  
+  leafletOutput("mymap" , width = "100%", height = 710),
+  absolutePanel(top = 70, left = 70,
+                
+                dateInput(inputId="plumeDate", label="Analysis Date", 
+                          value = "2012-06-13", 
+                          min = "2005-08-05", max = "2015-12-30",
+                          format = "yyyy-mm-dd", startview = "month",
+                          language = "en", 
+                          width=110)
   ),
-  
-    
-  # conditionalPanel(
-  #   condition = "exists('m') == FALSE",
-  #   img(src="ajax_loader_blue_512.gif", align = "center")
-  # ),
-  # 
-  # Place the map onto the interface   
-  leafletOutput("mymap" , width = "75%", height = 700),
 
+  absolutePanel(bottom=100,
+      img(src="aqi_legend.png", align = "left", width=150)
+  ),
 
-  sidebarPanel(tags$a("Steven Brey | Ph.D. Student |", 
+  absolutePanel(bottom=0, left=20,
+                
+                tags$a("Steven Brey | Ph.D. Student |", 
                       href="http://atmos.colostate.edu/~sjbrey/"),
                tags$a("Source code |", 
                       href="https://github.com/stevenjoelbrey/HMSExplorer"),
                tags$a("NESDIS HMS |", 
                       href="http://www.ospo.noaa.gov/Products/land/hms.html"),
-               tags$a("EPA monitor data", 
+               tags$a("EPA monitor data |", 
                       href="http://aqsdr1.epa.gov/aqsweb/aqstmp/airdata/download_files.html"),
+               tags$a("Reported Fires Data", 
+                      href="https://www.fs.usda.gov/rds/archive/Product/RDS-2013-0009.3/"),
+               
                p("If you find bugs or have questions, please contact me via the link above"),
                h5(textOutput("counter")),
                
